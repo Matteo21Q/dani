03b0f2c1a5c828d96d0316cddd31e78ef6615f99
Matteo21Q <40173554+Matteo21Q@users.noreply.github.com>
2026-08-07 16:16:01 +0100

    Small fixes MOVER

--- R/test.NI.binary.R
+++ R/test.NI.binary.R
@@ -406,8 +406,8 @@ test.NI.binary <- function(n.control=NULL, n.experim=NULL, e.control=NULL, e.exp
       estimate<-as.numeric(test[3])
     } else if (test.type=="MOVER") {
       test<-moverci(e.experim,n.experim,e.control,n.control,contrast = "RR", level = 1-2*sig.level)
-      CI<-test$estimates[,c("lower","upper")]
-      estimate<-test$estimates[,c("est")]
+      CI<-as.numeric(test$estimates[,c("lower","upper")])
+      estimate<-as.numeric(test$estimates[,c("est")])
     } else if (test.type=="Gart.Nam") {
       test<-scasci(e.experim,n.experim,e.control,n.control,contrast = "RR", level = 1-2*sig.level)$estimates
       CI<-test[c(1,3)]
@@ -658,8 +658,8 @@ test.NI.binary <- function(n.control=NULL, n.experim=NULL, e.control=NULL, e.exp
       estimate<-as.numeric(test[3])
     } else if (test.type=="MOVER") {
       test<-moverci(e.experim,n.experim,e.control,n.control,contrast = "OR", level = 1-2*sig.level)
-      CI<-test$estimates[,c("lower","upper")]
-      estimate<-test$estimates[,c("est")]
+      CI<-as.numeric(test$estimates[,c("lower","upper")])
+      estimate<-as.numeric(test$estimates[,c("est")])
     } else if (test.type=="Gart.Nam") {
       test<-scasci(e.experim,n.experim,e.control,n.control,contrast = "OR", level = 1-2*sig.level)$estimates
       CI<-as.numeric(test[c(1,3)])
